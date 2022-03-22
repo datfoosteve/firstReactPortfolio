f4c5686f1dbc5e2d020cbd6c72001f5aa8bf707f
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-03-22 08:05:21 -0500

    todays the day i finish

--- src/yourdata.js
+++ src/yourdata.js
@@ -20,11 +20,11 @@ export default {
   name: "Stephen Puthenpurackal",
   headerTagline: [
     //Line 1 For Header
-    "Creating, integrating, Developing",
+    "Creating,",
     //Line 2 For Header
-    "products ideas movements, ",
+    "Integrating,",
     //Line 3 For Header
-    "wisdom expertise knowledge precision",
+    "Architecting",
   ],
   //   Header Paragraph
   headerParagraph:
@@ -42,8 +42,7 @@ export default {
       para:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      imageSrc: "https://picsum.photos/200/4000",
       //Project URL - Add Your Project Url Here
       url: "http://chetanverma.com/",
     },
@@ -52,8 +51,7 @@ export default {
       para:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      imageSrc: "https://picsum.photos/200/400",
       //Project URL - Add Your Project Url Here
       url: "http://chetanverma.com/",
     },
@@ -62,8 +60,7 @@ export default {
       para:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      imageSrc: "https://picsum.photos/200/400",
       //Project URL - Add Your Project Url Here
       url: "http://chetanverma.com/",
     },
@@ -72,8 +69,7 @@ export default {
       para:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      imageSrc: "https://picsum.photos/200/400",
       //Project URL - Add Your Project Url Here
       url: "http://chetanverma.com/",
     },
@@ -82,8 +78,7 @@ export default {
       para:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      imageSrc: "https://picsum.photos/200/400",
       //Project URL - Add Your Project Url Here
       url: "http://chetanverma.com/",
     },
@@ -118,8 +113,7 @@ export default {
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   aboutParaThree:
     "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  aboutImage:
-    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  aboutImage: "https://picsum.photos/200/400",
 
   //   End About Section ---------------------
 
@@ -177,11 +171,11 @@ export default {
     { img: githubIcon, url: "https://github.com/datfoosteve" },
     {
       img: codepenIcon,
-      url: "https://www.codepen.com/",
+      url: "https://www.codepen.com/datfoosteve",
     },
     {
       img: dribbbleIcon,
-      url: "https://dribbble.com/chetanverma",
+      url: "https://dribbble.com/stephenandarlie",
     },
     {
       img: instagramIcon,
