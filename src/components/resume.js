df36d40baa8d3295f0772fa4349c773a55c7c613
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-03-27 06:46:19 -0500

    i hope this works wtf

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,66 +1,33 @@
-import React, { useState } from "react"
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack"
-import "react-pdf/dist/esm/Page/AnnotationLayer.css"
-
-// @ts-ignore
-
-import PDF from "../../static/resume.pdf"
-
-const options = {
-  cMapUrl: "cmaps/",
-  cMapPacked: true,
-  standardFontDataUrl: "standard_fonts/",
-}
-
-export default function Sample() {
-  const [file, setFile] = useState("./Resume.pdf")
-  const [numPages, setNumPages] = useState(null)
-
-  function onFileChange(event) {
-    setFile(event.target.files[0])
-  }
-
-  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
-    setNumPages(nextNumPages)
-  }
-
+import React from "react"
+import Img from "../images/resume1024_1.png"
+import Img2 from "../images/resume1024_2.png"
+export default function Resume() {
   return (
     <div className="Example">
       <header>
         <object
-          data="../../static/resume.pdf"
+          data="https://github.com/datfoosteve/firstReactPortfolio/blob/3197ca7ed9575490dbdba08ef99b3c69fa2c8568/static/resume.pdf"
           type="application/pdf"
           width="100%"
           height="100%"
         >
-          <iframe
-            src="../../static/resume.pdf"
-            width="100%"
-            height="100%"
-          >
-            This browser does not support PDFs. Please download the PDF to view
-            it:
-            <a href="../../static/resume.pdf">Download PDF</a>
+          <iframe src="Img" width="100%" height="100%">
+            <iframe src="Img2" width="100%" height="100%">
+              <iframe
+                src="https://github.com/datfoosteve/firstReactPortfolio/blob/3197ca7ed9575490dbdba08ef99b3c69fa2c8568/static/resume.pdf"
+                width="100%"
+                height="100%"
+              >
+                This browser does not support PDFs. Please download the PDF to
+                view it:
+                <a href="https://github.com/datfoosteve/firstReactPortfolio/blob/3197ca7ed9575490dbdba08ef99b3c69fa2c8568/static/resume.pdf">
+                  Download PDF
+                </a>
+              </iframe>
+            </iframe>
           </iframe>
         </object>
       </header>
-      <div className="Example__container">
-        <div className="Example__container__load">
-          <label htmlFor="file">Load from file:</label>{" "}
-          <input onChange={onFileChange} type="file" />
-        </div>
-        <div className="Example__container__document">
-          <Document
-            file={PDF}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
-        </div>
-      </div>
     </div>
   )
 }
